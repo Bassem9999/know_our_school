6290f0a115502a970dafa8c3360740b80126a767
bassem <bassemramadan456@gmail.com>
2023-08-12 16:49:07 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    void play(){}
+    
     return MaterialApp(
       title: 'know our school',
       theme: ThemeData(
